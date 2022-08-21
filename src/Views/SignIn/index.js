2f8f6693650faf08ddd2e925a3aafa54002be9ed
faizazafar <faiza99826@gmail.com>
2022-08-22 03:02:16 +0500

    changes in design

--- src/Views/SignIn/index.js
+++ src/Views/SignIn/index.js
@@ -1,36 +1,29 @@
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
-import TextInputClass from '../../Components/TextInput';
+import TextInputClass from '../../components/TextInput';
 import {useState} from 'react';
-import CheckboxCustom from '../../Components/CheckboxCustom';
+import CheckboxCustom from '../../components/CheckboxCustom';
+import {ScrollView} from 'react-native-gesture-handler';
+import * as Animatable from 'react-native-animatable';
 
 const SignIn = props => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [checked, setChecked] = useState(false);
   return (
-    <View
-      style={{
-        flex: 1,
-        padding: 20,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-      }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}>
-        <Text
-          style={{
-            fontFamily: 'NanumMyeongjo-ExtraBold',
-            fontSize: 50,
-            color: 'black',
-          }}>
-          Login to your Account
-        </Text>
+    <ScrollView contentContainerStyle={{flex: 1, backgroundColor: '#d44f46'}}>
+      <View style={styles.header}>
+        <Text style={styles.text_header}>Welcome! </Text>
+        <Text style={styles.text_subtite}> Log In Into Your Account </Text>
       </View>
-      <View style={{marginTop: 30}}>
+      <Animatable.View style={styles.footer} animation="fadeInUpBig">
         <View
           style={{
             backgroundColor: '#fafafa',
@@ -49,6 +42,7 @@ const SignIn = props => {
             style={{
               padding: 5,
               paddingHorizontal: 20,
+              // fontFamily: 'JuliusSansOne-Regular',
             }}
             placeholder={'Email'}
             onChange={t => {
@@ -56,11 +50,12 @@ const SignIn = props => {
             }}
           />
         </View>
+
         <View
           style={{
             backgroundColor: '#fafafa',
             borderRadius: 20,
-            justifyContent: 'center',
+            // justifyContent: 'center',
             marginVertical: 10,
           }}>
           <TextInputClass
@@ -74,8 +69,10 @@ const SignIn = props => {
             }}
             pass={true}
             style={{
-              padding: 10,
+              padding: 5,
               paddingHorizontal: 20,
+              // fontFamily: 'JuliusSansOne-Regular',
+              // backgroundColor:"pink"
             }}
             placeholder={'Password'}
             onChange={t => {
@@ -83,15 +80,16 @@ const SignIn = props => {
             }}
           />
         </View>
+
         <View
           style={{
-            alignSelf: 'center',
             flexDirection: 'row',
-            marginVertical: 30,
+            marginLeft: 2,
+            marginVertical: 10,
           }}>
           <CheckboxCustom
-            size={20}
-            color={'#246bfd'}
+            size={15}
+            color={'#d44f46'}
             onPress={() => {
               setChecked(!checked);
             }}
@@ -99,96 +97,196 @@ const SignIn = props => {
           />
           <Text
             style={{
+              fontSize: 14,
+              color: 'black',
+              marginLeft: 5,
+              // marginTop:-3,
+              fontFamily: 'JuliusSansOne-Regular',
+            }}>
+            Remember me
+          </Text>
+          <TouchableOpacity>
+            <Text
+              style={{
+                fontSize: 13,
+                fontWeight: '300',
+                fontFamily: 'JuliusSansOne-Regular',
+                color: 'black',
+                marginLeft: 60,
+                // marginTop: 30,
+              }}>
+              Forgot Password ?
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            padding: 18,
+            backgroundColor: '#d44f46',
+            borderRadius: 20,
+            marginTop: 40,
+          }}>
+          <Text
+            style={{
+              fontSize: 18,
+              fontFamily: 'JuliusSansOne-Regular',
+              color: 'white',
+              textAlign: 'center',
+            }}>
+            Sign In
+          </Text>
+        </TouchableOpacity>
+        <Text
+          style={{
+            fontSize: 15,
+            fontFamily: 'JuliusSansOne-Regular',
+            color: 'black',
+            textAlign: 'center',
+            marginVertical: 10,
+          }}>
+          Or
+        </Text>
+
+        <TouchableOpacity
+        onPress={()=> props.navigation.navigate('SignUp')}
+          style={{
+            padding: 18,
+            borderRadius: 20,
+            borderColor: '#d44f46',
+            borderWidth: 2,
+          }}>
+          <Text
+            style={{
               fontSize: 18,
               fontFamily: 'JuliusSansOne-Regular',
               color: 'black',
               textAlign: 'center',
-              marginLeft: 5,
             }}>
-            Remember me
+            Sign Up
           </Text>
-        </View>
-      </View>
-      <TouchableOpacity
-        style={{padding: 20, backgroundColor: '#d44f46', borderRadius: 30}}>
+        </TouchableOpacity>
         <Text
           style={{
-            fontSize: 18,
+            fontSize: 15,
             fontFamily: 'JuliusSansOne-Regular',
-            color: 'white',
+            color: 'black',
             textAlign: 'center',
+            marginTop: 10,
           }}>
-          Sign In
+          Or continue with
         </Text>
-      </TouchableOpacity>
-      <Text
-        style={{
-          fontSize: 18,
-          fontFamily: 'JuliusSansOne-Regular',
-          color: '#246bfd',
-          textAlign: 'center',
-          marginTop: 30,
-        }}>
-        Forgot Password ?
-      </Text>
-
-      <Text
-        style={{
-          fontSize: 18,
-          fontFamily: 'JuliusSansOne-Regular',
-          color: 'black',
-          textAlign: 'center',
-          marginTop: 30,
-        }}>
-        Or continue with
-      </Text>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-          marginTop: 30,
-        }}>
-        <TouchableOpacity>
-          <Image
-            style={{width: 30, height: 30, resizeMode: 'contain'}}
-            source={require('../../Images/facebook.png')}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={{width: 40, height: 40, resizeMode: 'contain'}}
-            source={require('../../Images/google.png')}
-          />
-        </TouchableOpacity>
-      </View>
-      <Text
-        style={{
-          fontSize: 14,
-          fontFamily: 'JuliusSansOne-Regular',
-          color: 'black',
-          textAlign: 'center',
-          marginTop: 30,
-        }}>
-        Don't have an account?
-        <Text
-          onPress={() => {
-            props.navigation.navigate('SignUp');
-          }}
+        <View
           style={{
-            fontSize: 14,
-            fontFamily: 'JuliusSansOne-Regular',
-            color: '#246bfd',
-            textAlign: 'center',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
             marginTop: 30,
           }}>
-          {' '}
-          Sign up
-        </Text>
-      </Text>
-    </View>
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              borderBottomColor: '#4267B2',
+              borderWidth: 2,
+              borderRadius: 20,
+            }}>
+            <Image
+              style={{width: 40, height: 40, resizeMode: 'contain'}}
+              source={require('../../Images/download.jpg')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              borderBottomColor: '#d44f46',
+              borderWidth: 2,
+              borderRadius: 20,
+            }}>
+            <Image
+              style={{width: 40, height: 40, resizeMode: 'contain'}}
+              source={require('../../Images/google.png')}
+            />
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
+    </ScrollView>
   );
 };
 
 export default SignIn;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#d44f46',
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 50,
+  },
+  footer: {
+    flex: 5,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  text_header: {
+    color: '#fff',
+    fontFamily: 'JuliusSansOne-Regular',
+    fontSize: 30,
+  },
+  text_subtite: {
+    color: '#fff',
+    fontFamily: 'JuliusSansOne-Regular',
+    // fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 10,
+    marginLeft:-5
+  },
+  text_footer: {
+    color: '#05375a',
+    fontSize: 18,
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5,
+  },
+  actionError: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#FF0000',
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    paddingLeft: 10,
+    color: '#05375a',
+  },
+  errorMsg: {
+    color: '#FF0000',
+    fontSize: 14,
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  signIn: {
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
--- src/Views/SignUp/index.js
+++ src/Views/SignUp/index.js
@@ -1,36 +1,21 @@
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
-import TextInputClass from '../../Components/TextInput';
+import TextInputClass from '../../components/TextInput';
 import {useState} from 'react';
-import CheckboxCustom from '../../Components/CheckboxCustom';
-
+import CheckboxCustom from '../../components/CheckboxCustom';
+import {ScrollView} from 'react-native-gesture-handler';
+import * as Animatable from 'react-native-animatable';
 const SignUp = props => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [checked, setChecked] = useState(false);
   return (
-    <View
-      style={{
-        flex: 1,
-        padding: 20,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-      }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}>
-        <Text
-          style={{
-            fontFamily: 'NanumMyeongjo-ExtraBold',
-            fontSize: 50,
-            color: 'black',
-          }}>
-          Create your Account
-        </Text>
+    <ScrollView contentContainerStyle={{flex: 1, backgroundColor: '#d44f46'}}>
+      <View style={styles.header}>
+        <Text style={styles.text_header}>Welcome! </Text>
+        <Text style={styles.text_subtite}> Create Your Account</Text>
       </View>
-      <View style={{marginTop: 30}}>
+      <Animatable.View style={styles.footer} animation="fadeInUpBig">
         <View
           style={{
             backgroundColor: '#fafafa',
@@ -49,6 +34,7 @@ const SignUp = props => {
             style={{
               padding: 5,
               paddingHorizontal: 20,
+              // fontFamily: 'JuliusSansOne-Regular',
             }}
             placeholder={'Email'}
             onChange={t => {
@@ -56,11 +42,42 @@ const SignUp = props => {
             }}
           />
         </View>
+
+        
+        
         <View
           style={{
             backgroundColor: '#fafafa',
             borderRadius: 20,
-            justifyContent: 'center',
+            marginTop:10
+          }}>
+          <TextInputClass
+            image={require('../../assets/lock.png')}
+            imageStyle={{
+              width: 20,
+              height: 20,
+              justifyContent: 'center',
+              alignSelf: 'center',
+              tintColor: '#9e9e9e',
+            }}
+            pass={true}
+            style={{
+              padding: 5,
+              paddingHorizontal: 20,
+              // fontFamily: 'JuliusSansOne-Regular',
+              // backgroundColor:"pink"
+            }}
+            placeholder={'Mobile Number'}
+            onChange={t => {
+              setPassword(t);
+            }}
+          />
+        </View>
+        <View
+          style={{
+            backgroundColor: '#fafafa',
+            borderRadius: 20,
+            // justifyContent: 'center',
             marginVertical: 10,
           }}>
           <TextInputClass
@@ -74,8 +91,10 @@ const SignUp = props => {
             }}
             pass={true}
             style={{
-              padding: 10,
+              padding: 5,
               paddingHorizontal: 20,
+              // fontFamily: 'JuliusSansOne-Regular',
+              // backgroundColor:"pink"
             }}
             placeholder={'Password'}
             onChange={t => {
@@ -85,100 +104,197 @@ const SignUp = props => {
         </View>
         <View
           style={{
-            alignSelf: 'center',
-            flexDirection: 'row',
-            marginVertical: 30,
+            backgroundColor: '#fafafa',
+            borderRadius: 20,
+            // justifyContent: 'center',
+            // marginVertical: 10,
           }}>
-          <CheckboxCustom
-            size={20}
-            color={'#246bfd'}
-            onPress={() => {
-              setChecked(!checked);
+          <TextInputClass
+            image={require('../../assets/lock.png')}
+            imageStyle={{
+              width: 20,
+              height: 20,
+              justifyContent: 'center',
+              alignSelf: 'center',
+              tintColor: '#9e9e9e',
             }}
-            status={checked}
+            pass={true}
+            style={{
+              padding: 5,
+              paddingHorizontal: 20,
+              // fontFamily: 'JuliusSansOne-Regular',
+              // backgroundColor:"pink"
+            }}
+            placeholder={'Confirm Password'}
+            onChange={t => {
+              setPassword(t);
+            }}
           />
+        </View>
+
+        <TouchableOpacity
+          style={{
+            padding: 18,
+            backgroundColor: '#d44f46',
+            borderRadius: 20,
+            marginTop: 40,
+          }}>
           <Text
             style={{
               fontSize: 18,
               fontFamily: 'JuliusSansOne-Regular',
-              color: 'black',
+              color: 'white',
               textAlign: 'center',
-              marginLeft: 5,
             }}>
-            Remember me
+            Sign Up
           </Text>
-        </View>
-      </View>
+        </TouchableOpacity>
+        <Text
+          style={{
+            fontSize: 15,
+            fontFamily: 'JuliusSansOne-Regular',
+            color: 'black',
+            textAlign: 'center',
+            marginVertical: 10,
+          }}>
+          Or
+        </Text>
 
-      <TouchableOpacity
-        style={{padding: 20, backgroundColor: '#d44f46', borderRadius: 30}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            // marginTop: 5,
+          }}>
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              borderBottomColor: '#4267B2',
+              borderWidth: 2,
+              borderRadius: 20,
+            }}>
+            <Image
+              style={{width: 40, height: 40, resizeMode: 'contain'}}
+              source={require('../../Images/download.jpg')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              borderBottomColor: '#d44f46',
+              borderWidth: 2,
+              borderRadius: 20,
+            }}>
+            <Image
+              style={{width: 40, height: 40, resizeMode: 'contain'}}
+              source={require('../../Images/google.png')}
+            />
+          </TouchableOpacity>
+
+        </View>
+
+<View style={{flexDirection:"row" ,justifyContent:"center"}}>
         <Text
           style={{
-            fontSize: 18,
+            fontSize: 12,
             fontFamily: 'JuliusSansOne-Regular',
-            color: 'white',
+            color: 'black',
             textAlign: 'center',
+            marginTop:30,
           }}>
-          Sign In
+          Already have an account? 
         </Text>
-      </TouchableOpacity>
-      <Text
-        style={{
-          fontSize: 18,
-          fontFamily: 'JuliusSansOne-Regular',
-          color: 'black',
-          textAlign: 'center',
-          marginTop: 30,
-        }}>
-        Or continue with
-      </Text>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-          marginTop: 30,
-        }}>
-        <TouchableOpacity>
-          <Image
-            style={{width: 30, height: 30, resizeMode: 'contain'}}
-            source={require('../../Images/facebook.png')}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            style={{width: 40, height: 40, resizeMode: 'contain'}}
-            source={require('../../Images/google.png')}
-          />
-        </TouchableOpacity>
-      </View>
-      <Text
-        style={{
-          fontSize: 14,
-          fontFamily: 'JuliusSansOne-Regular',
-          color: 'black',
-          textAlign: 'center',
-          marginTop: 30,
-        }}>
-        Already have an account?
-        <Text
-          onPress={() => {
-            props.navigation.navigate('SignIn');
-          }}
-          style={{
-            fontSize: 14,
+        <TouchableOpacity
+        onPress={()=>props.navigation.navigate('SignIn')}>
+          <Text   style={{
+            fontSize: 12,
             fontFamily: 'JuliusSansOne-Regular',
-            color: 'blue',
+            color: '#d44f46',
             textAlign: 'center',
-            marginTop: 30,
-          }}>
-          {' '}
-          Sign in
-        </Text>
-      </Text>
-    </View>
+            marginTop:30,
+          }}> {''}Sign In</Text>
+        </TouchableOpacity>
+        </View>
+      </Animatable.View>
+    </ScrollView>
   );
 };
 
 export default SignUp;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#d44f46',
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 50,
+  },
+  footer: {
+    flex: 5,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  text_header: {
+    color: '#fff',
+    fontFamily: 'JuliusSansOne-Regular',
+    fontSize: 30,
+  },
+  text_subtite: {
+    color: '#fff',
+    fontFamily: 'JuliusSansOne-Regular',
+    // fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 10,
+    marginLeft:-5
+  },
+  text_footer: {
+    color: '#05375a',
+    fontSize: 18,
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5,
+  },
+  actionError: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#FF0000',
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    paddingLeft: 10,
+    color: '#05375a',
+  },
+  errorMsg: {
+    color: '#FF0000',
+    fontSize: 14,
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  signIn: {
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
