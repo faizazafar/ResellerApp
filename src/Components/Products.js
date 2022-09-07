71c1b8925fbd5be00b2f6b1e721ad1c18eab7e7f
faizazafar <faiza99826@gmail.com>
2022-09-08 03:30:27 +0500

    product deatils in progress

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -5,6 +5,17 @@ const Products= [
         productName: 'Casual Dress',
         price: '43',
         image: require('../assets/dress3.jpg'),
+        subTitle:"Perfect Formal Wera",
+        details:[
+          'Embroided Velvet',
+          'Comfortable Feel',
+          'Fit for occaions',
+          'EasternLook'
+        ],
+
+        size:"Available size 6,7,8,9,10",
+        note:"NOTE :Lorem ipsum dolor sit amet. Et dolorem enim ut mollitia recusandae 33 quia commodi. Et labore fuga a nihil doloremque At velit culpa 33 fugit beatae in sequi asperiores in nemo rerum aut dicta repellendus.",
+        productCode:"MZ154SHGD",
         productImageList:[
 
           require('../assets/dress4.jpg'),
@@ -19,13 +30,24 @@ const Products= [
         productName: 'Formal Dress',
         price: '60',
         image: require('../assets/dress4.jpg'),
+        subTitle:"Perfect Casual Wera",
         productImageList:[
 
           require('../assets/dress4.jpg'),
           require('../assets/dress3.jpg'),
           require('../assets/cloth.jpg')
 
-        ]
+        ],
+        details:[
+          'Embroided Velvet',
+          'Comfortable Feel',
+          'Fit for occaions',
+          'EasternLook'
+        ],
+
+        size:"Available size 6,7,8,9,10",
+        note:"NOTE :Lorem ipsum dolor sit amet. Et dolorem enim ut mollitia recusandae 33 quia commodi. Et labore fuga a nihil doloremque At velit culpa 33 fugit beatae in sequi asperiores in nemo rerum aut dicta repellendus.",
+        productCode:"ABCSHGD",
       },
 
       {
@@ -33,13 +55,24 @@ const Products= [
         productName: 'Casual Dress',
         price: '43',
         image: require('../assets/cloth.jpg'),
+        subTitle:"Perfect Casual Wjs",
         productImageList:[
 
           require('../assets/dress4.jpg'),
           require('../assets/dress3.jpg'),
           require('../assets/cloth.jpg')
 
-        ]
+        ],
+        details:[
+          'Embroided Velvet',
+          'Comfortable Feel',
+          'Fit for occaions',
+          'EasternLook'
+        ],
+
+        size:"Available size 6,7,8,9,10",
+        note:"NOTE :Lorem ipsum dolor sit amet. Et dolorem enim ut mollitia recusandae 33 quia commodi. Et labore fuga a nihil doloremque At velit culpa 33 fugit beatae in sequi asperiores in nemo rerum aut dicta repellendus.",
+        productCode:"AHGYGSHGD",
       },
   
       {
@@ -53,7 +86,19 @@ const Products= [
           require('../assets/dress3.jpg'),
           require('../assets/cloth.jpg')
 
-        ]
+        ],
+        details:[
+          'Embroided Velvet',
+          'Comfortable Feel',
+          'Fit for occaions',
+          'EasternLook'
+        ],
+
+        size:"Available size 6,7,8,9,10",
+        note:"NOTE :Lorem ipsum dolor sit amet. Et dolorem enim ut mollitia recusandae 33 quia commodi. Et labore fuga a nihil doloremque At velit culpa 33 fugit beatae in sequi asperiores in nemo rerum aut dicta repellendus.",
+        productCode:"AB76876876CSHGD",
+        subTitle:"Perfect Casual Wjs",
+
       },
       
       {
@@ -61,6 +106,25 @@ const Products= [
         productName: 'Dress',
         price: '500',
         image: require('../assets/dress5.jpg'),
+        productImageList:[
+
+          require('../assets/dress4.jpg'),
+          require('../assets/dress3.jpg'),
+          require('../assets/cloth.jpg')
+
+        ],
+        details:[
+          'Embroided Velvet',
+          'Comfortable Feel',
+          'Fit for occaions',
+          'EasternLook'
+        ],
+
+        size:"Available size 6,7,8,9,10",
+        note:"NOTE :Lorem ipsum dolor sit amet. Et dolorem enim ut mollitia recusandae 33 quia commodi. Et labore fuga a nihil doloremque At velit culpa 33 fugit beatae in sequi asperiores in nemo rerum aut dicta repellendus.",
+        productCode:"AB76876876CSHGD",
+        subTitle:"Perfect Casual Wjs",
+
       },
   
       {
@@ -68,13 +132,24 @@ const Products= [
         productName: 'Formal Dress',
         price: '900',
         image: require('../assets/dress2.jpg'),
+        subTitle:"Perfect Casual Wjs",
         productImageList:[
 
           require('../assets/dress4.jpg'),
           require('../assets/dress3.jpg'),
           require('../assets/cloth.jpg')
 
-        ]
+        ],
+        details:[
+          'Embroided Velvet',
+          'Comfortable Feel',
+          'Fit for occaions',
+          'EasternLook'
+        ],
+
+        size:"Available size 6,7,8,9,10",
+        note:"NOTE :Lorem ipsum dolor sit amet. Et dolorem enim ut mollitia recusandae 33 quia commodi. Et labore fuga a nihil doloremque At velit culpa 33 fugit beatae in sequi asperiores in nemo rerum aut dicta repellendus.",
+        productCode:"AB76876876CSHGD",
       }
 ]
   export default Products
--- src/Views/ProductDetails/index.js
+++ src/Views/ProductDetails/index.js
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, View , FlatList, ScrollView, Image, Dimensions
+import { StyleSheet, Text, View , FlatList, ScrollView, Image, Dimensions , TouchableOpacity
 , Animated} from 'react-native'
 import React, { useEffect  , useState} from 'react'
 import Header from '../../components/Header'
 import {getProduct} from '../../components/Products'
+import { Rating } from 'react-native-ratings'
 
 const ProductDetails = ({route}) => {
     const { productId } = route.params
@@ -14,6 +15,8 @@ const ProductDetails = ({route}) => {
   }, []);
 
   console.log(product)
+  const star_unfill = '../../assets/star-unfill.png';
+
 
   const width = Dimensions.get('window').width
  const scrollX = new Animated.Value(0);
@@ -42,6 +45,7 @@ const ProductDetails = ({route}) => {
     contentContainerStyle={{
       flexGrow: 1,
       backgroundColor: 'white',
+      paddingBottom: 30,
     }}>
               <Header />
 
@@ -55,12 +59,12 @@ renderItem={renderProduct}  onScroll={Animated.event(
 />
 <View
             style={{
-              width: '100%',
+              // width: '100%',
               flexDirection: 'row',
               alignItems: 'center',
               justifyContent: 'center',
               marginBottom: 16,
-              marginTop: 32,
+              // marginTop: 32,
             }}>
             {product.productImageList
               ? product.productImageList.map((data, index) => {
@@ -79,12 +83,132 @@ renderItem={renderProduct}  onScroll={Animated.event(
                         opacity,
                         marginHorizontal: 4,
                         borderRadius: 100,
+                        // bottom:310
                       }}></Animated.View>
                   );
                 })
               : null}
+
+              
           </View>
-        <View style={styles.infoContainer}>
+          <View style={styles.infoContainer}>
+          <View style={{flexDirection:'row',
+        justifyContent:"space-between"}}>
+          <Text style={styles.price}>$ {product.price}</Text> 
+          <TouchableOpacity>
+                    <Image
+                      source={require('../../assets/shares.png')}
+                      style={{width: 20, height: 20, tintColor: '#d44f46'}}></Image>
+                  </TouchableOpacity>
+                  {/* <Text style={styles.name}>{product.productName}</Text> */}
+
+
+          </View>
+          <Text style={styles.name}>{product.productName}</Text>
+          <Text style={styles.name}>{product.subTitle}</Text>
+
+          <View style={{flexDirection:"row" , justifyContent:"space-between"}}>
+          <Text>Reviews</Text>
+          <Rating starContainerStyle={{ justifyContent:"space-between"}}	
+  ratingCount={5}
+  imageSize={20}
+  startingValue={0}
+/>
+          </View>
+
+          <View style={{flexDirection:"row" , width:"100%" }}>
+            {/* <View style={{justifyContent:""}}> */}
+            <TouchableOpacity style={{marginRight:10}}>
+         <Image source={require('../../assets/copy2.png')}  tintColor={'black'}
+         style={{ width:20 , height:20  }}/>
+
+
+         </TouchableOpacity>
+         <Text>jsbxj</Text>
+
+         {/* </View> */}
+         
+         <TouchableOpacity  >
+         <Image source={require('../../assets/down2.png')}  tintColor={'black'}
+         style={{ width:20 , height:20 , left:200 }}/>
+         </TouchableOpacity>
+
+         <Text style={{left:210}}>jsbxj</Text>
+
+          </View>
+
+          <View style={styles.verticleLine}></View>
+
+          <Text>Delivery</Text>
+
+          <Text>Product will be delivered in 2 -3 days</Text>
+
+
+          <View style={styles.verticleLineTwo}></View>
+
+          <Text>Details</Text>
+
+          {product?.details && product?.details.map((e)=>{ return <Text>{e}</Text> })}
+          <Text>{`${product.size}\n`}</Text>
+
+          <Text>{product.note}</Text>
+
+          <Text>{`Product Code : ${product.productCode}`}</Text>
+
+          <View style={styles.verticleLineTwo}></View>
+<Text>Refund Policy</Text>
+
+<Text>7 Days Return</Text>
+<Text>Warranty not available</Text>
+
+<View style={{flexDirection:"row" , justifyContent:"space-evenly", flex:1}}>
+
+<TouchableOpacity
+          style={{
+            padding: 10,
+            backgroundColor: '#d44f46',
+            borderRadius: 5,
+            marginTop: 40,
+            width:'45%',
+          
+          }}>
+          <Text
+            style={{
+              fontSize: 15,
+              fontFamily: 'JuliusSansOne-Regular',
+              color: 'white',
+              textAlign: 'center',
+            }}>
+            Order Now
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            padding: 10,
+            backgroundColor: '#d44f46',
+            borderRadius: 5,
+            marginTop: 40,
+            width:'45%'
+
+          
+          }}>
+          <Text
+            style={{
+              fontSize: 15,
+              fontFamily: 'JuliusSansOne-Regular',
+              color: 'white',
+              textAlign: 'center',
+            }}>
+           Add to Bag
+          </Text>
+        </TouchableOpacity>
+</View>
+
+          </View>
+
+{/* 
+         <View style={styles.infoContainer}>
           <Text style={styles.name}>{product.productName}</Text>
           <Text style={styles.price}>$ {product.price}</Text> 
           <Text style={styles.description}>{product.description}</Text> 
@@ -99,7 +223,7 @@ renderItem={renderProduct}  onScroll={Animated.event(
           <Text style={styles.name}>{product.productName}</Text>
           <Text style={styles.price}>$ {product.price}</Text> 
           <Text style={styles.description}>{product.description}</Text> 
-        </View> 
+        </View>   */}
 
 
 
@@ -145,4 +269,19 @@ const styles = StyleSheet.create({
       color: '#787878',
       marginBottom: 16,
     },
+    verticleLine: {
+      // height: '90%',
+      height:3,
+      width: '100%',
+      backgroundColor: '#D3D3D3',
+      marginTop: 3,
+    },
+
+    verticleLineTwo: {
+      // height: '90%',
+      height:1,
+      width: '100%',
+      backgroundColor: '#D3D3D3',
+      marginTop: 3,
+    }
   });
